66b23788ebad26bef80ee3af8df2018ead961d50
MrGblox <grobolxx@gmail.com>
2025-08-17 12:33:31 +0700

    Update Sample.lua

--- Sample.lua
+++ Sample.lua
@@ -1,74 +1,39 @@
-local Rayfield = loadstring(game:HttpGet('https://sirius.menu/rayfield'))()
-  local Window = Rayfield:CreateWindow({
-   Name = "Car Driving Indonesian Scripts",
-   Icon = 0, -- Icon in Topbar. Can use Lucide Icons (string) or Roblox Image (number). 0 to use no icon (default).
-   LoadingTitle = "Car Driving Indonesian Script",
-   LoadingSubtitle = "By - X4 Hub",
-   ShowText = "@X4 Hub", -- for mobile users to unhide rayfield, change if you'd like
-   Theme = "Default", -- Check https://docs.sirius.menu/rayfield/configuration/themes
+local Luna = loadstring(game:HttpGet("https://raw.githubusercontent.com/Nebula-Softworks/Luna-Interface-Suite/refs/heads/master/source.lua", true))()
+local Window = Luna:CreateWindow({
+	Name = "X4 Hub Car Driving Indonesian", -- This Is Title Of Your Window
+	Subtitle = nil, -- A Gray Subtitle next To the main title.
+	LogoID = "82795327169782", -- The Asset ID of your logo. Set to nil if you do not have a logo for Luna to use.
+	LoadingEnabled = true, -- Whether to enable the loading animation. Set to false if you do not want the loading screen or have your own custom one.
+	LoadingTitle = "X4 Hub", -- Header for loading screen
+	LoadingSubtitle = "Car Driving Indonesian Script", -- Subtitle for loading screen
 
-   ToggleUIKeybind = "K", -- The keybind to toggle the UI visibility (string like "K" or Enum.KeyCode)
+	ConfigSettings = {
+		RootFolder = nil,  
+		ConfigFolder = "X4 Hub" 
+	},
 
-   DisableRayfieldPrompts = false,
-   DisableBuildWarnings = false, -- Prevents Rayfield from warning when the script has a version mismatch with the interface
-
-   ConfigurationSaving = {
-      Enabled = true,
-      FolderName = nil, -- Create a custom folder for your hub/game
-      FileName = "Big Hub"
-   },
-
-   Discord = {
-      Enabled = false, -- Prompt the user to join your Discord server if their executor supports it
-      Invite = "noinvitelink", -- The Discord invite code, do not include discord.gg/. E.g. discord.gg/ ABCD would be ABCD
-      RememberJoins = true -- Set this to false to make them join the discord every time they load it up
-   },
-
-   KeySystem = true, -- Set this to true to use our key system
-   KeySettings = {
-      Title = "CDID Script Key",
-      Subtitle = "You need a key to inject this script",
-      Note = "Get the key in X4 HUB discord", -- Use this to tell the user how to get a key
-      FileName = "Key", -- It is recommended to use something unique as other scripts using Rayfield may overwrite your key file
-      SaveKey = true, -- The user's key will be saved, but if you change the key, they will be unable to use your script
-      GrabKeyFromSite = false, -- If this is true, set Key below to the RAW site you would like Rayfield to get the key from
-      Key = {"Hello"} -- List of keys that will be accepted by the system, can be RAW file links (pastebin, github etc) or simple strings ("hello","key22")
-   }
+	KeySystem = false, -- As Of Beta 6, Luna Has officially Implemented A Key System!
+	KeySettings = {
+		Title = "Luna Example Key",
+		Subtitle = "Key System",
+		Note = "Best Key System Ever! Also, Please Use A HWID Keysystem like Pelican, Luarmor etc. that provide key strings based on your HWID since putting a simple string is very easy to bypass",
+		SaveInRoot = false, -- Enabling will save the key in your RootFolder (YOU MUST HAVE ONE BEFORE ENABLING THIS OPTION)
+		SaveKey = true, -- The user's key will be saved, but if you change the key, they will be unable to use your script
+		Key = {"Example Key"}, -- List of keys that will be accepted by the system, please use a system like Pelican or Luarmor that provide key strings based on your HWID since putting a simple string is very easy to bypass
+		SecondAction = {
+			Enabled = true, -- Set to false if you do not want a second action,
+			Type = "Link", -- Link / Discord.
+			Parameter = "" -- If Type is Discord, then put your invite link (DO NOT PUT DISCORD.GG/). Else, put the full link of your key system here.
+		}
+	}
 })
-  Rayfield:Notify({
-   Title = "Thx For Using This Script",
-   Content = "Have some fun with this script :)",
-   Duration = 0.5,
-   Image = 4483362458,
+Window:CreateHomeTab({
+	SupportedExecutors = {Support all executor}, -- A Table Of Executors Your Script Supports. Add strings of the executor names for each executor.
+	DiscordInvite = "1234", -- The Discord Invite Link. Do Not Include discord.gg/ | Only Include the code.
+	Icon = 1, -- By Default, The Icon Is The Home Icon. If You would like to change it to dashboard, replace the interger with 2
 })
-  local MainTab = Window:CreateTab("Main", false)-- Title, Image
-  local Section = MainTab:CreateSection("Player")
-  local Slider = MainTab:CreateSlider({
-   Name = "Slider Example",
-   Range = {0, 200},
-   Increment = 10,
-   Suffix = "Bananas",
-   CurrentValue = 10,
-   Flag = "Slider1", 
-   Callback = function(Value)
-   game.Player.LocalPlayer.Humanoid.WalkSpeed = (Value)
-   end,
-})
-  local Button = Tab:CreateButton({
-   Name = "Infinity Jump",
-   Callback = function()
-   -- The function that takes place when the button is pressed
-   end,
-})
-  local Toggle = Tab:CreateToggle({
-   Name = "Toggle Example",
-   CurrentValue = false,
-   Flag = "Toggle1", -- A flag is the identifier for the configuration file, make sure every element has a different flag if you're using configuration saving to ensure no overlaps
-   Callback = function(Value)
-
-   end,
-})
-  
-  
-  if not game.PlaceId = then
-    
+local MainTab = Window:CreateTab({
+	Name = "Main",
+	Icon = "74308559916698",
+	ImageSource = "Material",
+	ShowTitle = false

5347f31c183e890f1c1693ff3c46f07399551867
MrGblox <grobolxx@gmail.com>
2025-10-02 23:58:58 +0700

    Update 44hub.lua

--- 44hub.lua
+++ 44hub.lua
@@ -1,40 +1,26 @@
 local WindUI = loadstring(game:HttpGet("https://github.com/Footagesus/WindUI/releases/latest/download/main.lua"))()
 local Window = WindUI:CreateWindow({
-    Title = "44 Universal Hub",
-    Icon = "earth",
-    Author = "By | 4izfr",
-    Folder = "cowoganteng",
+    Title = "44 Hub | Universal",
+    Icon = "earth", -- lucide icon
+    Author = "By | @4izfr",
+    Folder = "game",
     
     Size = UDim2.fromOffset(580, 460),
     MinSize = Vector2.new(560, 350),
     MaxSize = Vector2.new(850, 560),
-    Theme = "Dark",
+    Transparent = false,
+    Theme = "Rainbow",
     Resizable = true,
     SideBarWidth = 200,
     BackgroundImageTransparency = 0.42,
-    ScrollBarEnabled = false,
+    HideSearchBar = false,
+    ScrollBarEnabled = true,
     
     User = {
         Enabled = true,
+        Anonymous = false,
         Callback = function()
             print("clicked")
         end,
     },
 })
-local MainSection = Window:Section({Title = "- Main",Opened = true,})
--- HomeTab
-local HomeTab = MainSection:Tab({Title = "home",Icon = "house",Locked = false,})
--- home tab contents 
-HomeTab:Paragraph({
-    Title = "Welcome to our universal script",
-    Desc = "I hope those of you who use our script like our script, if there is a problem with this script, you can join our discord server to report any bugs that occur.",
-    Locked = false,
-    Buttons = {
-        {
-            Icon = "copy",
-            Title = "join our discord :d",
-            Callback = setclipboard("https://discord.gg/44hub") end,
-        }
-    }
-})
-

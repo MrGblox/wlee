c6a0b734597321bacdb3e7ceb2fc8ee83c372600
MrGblox <grobolxx@gmail.com>
2025-10-05 14:02:37 +0700

    Update 44hub.lua

--- 44hub.lua
+++ 44hub.lua
@@ -17,11 +17,13 @@ local Window = WindUI:CreateWindow({
     ScrollBarEnabled = true,
     
   User = {Enabled = true, Anonymous = false, Callback = function() local player = game.Players.LocalPlayer local username = player.Name if setclipboard then setclipboard(username) WindUI:Notify({Title = "Copied!", Content = "Username kamu telah disalin: " .. username, Duration = 3, Icon = "check"}) print("✅ Copied username:", username) else WindUI:Notify({Title = "Gagal", Content = "Executor kamu tidak mendukung setclipboard!", Duration = 3, Icon = "x"}) warn("❌ setclipboard tidak tersedia.") end end,}})
-Window:Tag({
+Window:Space()
+  Window:Tag({
     Title = "v1.6.4",
     Color = Color3.fromHex("#30ff6a"),
     Radius = 12, 
 })
+Window:Space()
 Window:Tag({
     Title = "v1.6.4",
     Color = Color3.fromHex("#30ff6a"),

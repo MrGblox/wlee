e19213f697a1af7cb5dff9bffea538bd0d89c23b
MrGblox <grobolxx@gmail.com>
2025-10-05 13:51:48 +0700

    Update 44hub.lua

--- 44hub.lua
+++ 44hub.lua
@@ -1,7 +1,7 @@
 local WindUI = loadstring(game:HttpGet("https://github.com/Footagesus/WindUI/releases/latest/download/main.lua"))()
 local Window = WindUI:CreateWindow({
     Title = "44 HUB | UNIVERSAl",
-    Icon = "lucide:world", -- lucide icon
+    Icon = "earth",
     Author = "BY | 4izfr",
     Folder = "MySuperHub",
 

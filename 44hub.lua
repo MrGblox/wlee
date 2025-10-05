8f965785f23752fe4cf01117356ab28b52fd8048
MrGblox <grobolxx@gmail.com>
2025-10-05 13:29:26 +0700

    Update 44hub.lua

--- 44hub.lua
+++ 44hub.lua
@@ -1,9 +1,9 @@
 local WindUI = loadstring(game:HttpGet("https://github.com/Footagesus/WindUI/releases/latest/download/main.lua"))()
 local Window = WindUI:CreateWindow({
-    Title = "44 Hub | Universal",
+    Title = "44 HUB | UNIVERSAL",
     Icon = "earth", -- lucide icon
     Author = "By | @4izfr",
-    Folder = "game",
+    Folder = "44HUBONTOP",
     
     Size = UDim2.fromOffset(580, 460),
     MinSize = Vector2.new(560, 350),
@@ -24,19 +24,28 @@ local Window = WindUI:CreateWindow({
         end,
     },
 })
+Window:CreateTopbarButton({"Discord", "clipboard-paste", function() setclippboard("coming soon"),})
+Window:Tag({Title = "Working",Color = Color3.fromHex("#01fb00"),Radius = 0,})
+Window:Tag({Title = "Beta Version",Color = Color3.fromHex("#fbec00"),Radius = 0,})
+WindUI:Notify({Title = "Welcome to the 44 HUB | UNIVERSAL",Content = "Thx for using our script, enjoy it :d",Duration = 120,Icon = "bell",})
 -- Main Section
-local MainSection = Window:Section({Title = "•Main",Opened = true,})
-local MainTab = MainSection:Tab({Title = "Home",Icon = "house",Locked = false,})
--- Main tab content
-local Paragraph = Tab:Paragraph({
-    Title = "Enjoy our script!",
-    Desc = "",
+local MainSection = Window:Section({Title = "Main",Opened = true,})
+
+-- Main Tabs
+local HomeTab = Window:Tab({Title = "Home",Icon = "house",Locked = false,})
+HomeTab:Paragraph({
+    Title = "Welcome to 44 HUB | UNIVERSAL :D",
+    Desc = "hopefully you like the script we created, if when you use this script you experience an error, you can report the bug to our discord, and you can also join our discord server to find friends or look for the script you want",
+    Image = "smile",
+    ImageSize = 20,
+    Thumbnail = "",
+    ThumbnailSize = 80,
     Locked = false,
     Buttons = {
         {
-            Icon = "copy",
-            Title = "Join our discord :3",
-            Callback = setclipboard("kontolkau") end,
+            Icon = "external-link",
+            Title = "Join our discord",
+            Callback = function() setclippboard("coming soon") end,
         }
     }
 })

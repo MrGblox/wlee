5cf959516436be15e4819363e46c88120173703c
MrGblox <grobolxx@gmail.com>
2025-10-11 19:25:54 +0700

    Update 44hub.lua

--- 44hub.lua
+++ 44hub.lua
@@ -18,9 +18,9 @@ local Window = WindUI:CreateWindow({
   
     User = {Enabled = true, Anonymous = false, Callback = function() local player = game.Players.LocalPlayer local username = player.Name if setclipboard then setclipboard(username) WindUI:Notify({Title = "Copied!", Content = "your username has been copied!: " .. username, Duration = 3, Icon = "check"}) print("✅ Copied username:", username) else WindUI:Notify({Title = "fail", Content = "can't copy your username!", Duration = 3, Icon = "x"}) end end,}
 })
-Window:CreateTopbarButton("Discord", "copy",    function() setclipboard("discord.gg") end,  990)
+Window:CreateTopbarButton("Discord", "copy",    function() setclipboard("https://discord.gg/kyGs6tuBQ8") end,  990)
 local HomeTab = Window:Tab({Title = "Home",Icon = "house",Locked = false,})
-local Paragraph = HomeTab:Paragraph({
+Paragraph = HomeTab:Paragraph({
     Title = "Welxcome to 44hub | Universal !",
     Desc = "Enjoy the script we made! If you find any bugs here, you can report them directly to our Discord :D",
     Thumbnail = "https://tr.rbxcdn.com/180DAY-1ef078e0daa7568fa3a0088e9ee6b6b2/768/432/Image/Webp/noFilter",
@@ -30,7 +30,7 @@ local Paragraph = HomeTab:Paragraph({
         {
             Icon = "copy",
             Title = "Join our discord ^^",
-            Callback = function() setclipboard("discord.gg") end,
+            Callback = function() setclipboard("https://discord.gg/kyGs6tuBQ8") end,
         }
     }
 })
